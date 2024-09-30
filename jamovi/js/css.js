c2de145f49491a5b384fcd0a90f0966b27aaa3fb
MAgojam <magosti@gmail.com>
2024-09-30 18:36:39 +0200

    Added keyboard shortcut and minor changes

--- jamovi/js/css.js
+++ jamovi/js/css.js
@@ -1,7 +1,7 @@
-
 const css = `
 
-#butsf {
+/* Stile comune per entrambi i bottoni */
+.button-style {
     display: inline-block;
     padding: 3px 5px;
     font-size: 14px;
@@ -18,19 +18,38 @@ const css = `
     cursor: pointer;
 }
 
-#butsf:hover {
+.button-style:hover {
     background-color: #3cb0fd;
     background-image: linear-gradient(to bottom, #3cb0fd, #3498db);
 }
 
-#butsf:active {
-  background-color: #3498db;
-  background-image: linear-gradient(to bottom, #3498db, #2980b9);
-  box-shadow: 0 3px #DADADA;
-  transform: translateY(4px);
+.button-style:active {
+    background-color: #3498db;
+    background-image: linear-gradient(to bottom, #3498db, #2980b9);
+    box-shadow: 0 3px #DADADA;
+    transform: translateY(4px);
 }
 
+/* Personalizzazioni specifiche per ciascun bottone */
+#butsf-file span, #butsf-reshape span {
+    text-decoration: underline;
+}
+
+/* Stili per il tooltip personalizzato */
+.custom-tooltip {
+    display: none;
+    position: absolute;
+    background-color: yellow;
+    color: black;
+    padding: 5px;
+    border-radius: 4px;
+    z-index: 1000;
+    white-space: nowrap;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+    border: 1px solid black;
+}
 `;
+
 let node = document.createElement('style');
 node.innerHTML = css;
 document.body.appendChild(node);
--- jamovi/js/jrmergecols.js
+++ jamovi/js/jrmergecols.js
@@ -3,38 +3,109 @@ require('./css');
 
 module.exports = {
 
-    fleChs_creating: function(ui) {
+    loaded: function(ui) {
 
+        // Aggiungere listener per la combinazione di tasti Ctrl+F per selezionare un file
+        Keyboard?.addKeyboardListener?.('Ctrl+KeyF', () => {
+            let fileInput = ui.fleChs.$el.find('span#butsf-file');
+            if (fileInput.length) {
+                fileInput.trigger('click');
+            }
+        }, 'Shortcut per selezionare un file');
+
+        // Aggiungere listener per la combinazione di tasti Ctrl+R per il bottone reshape
+        Keyboard?.addKeyboardListener?.('Ctrl+KeyR', () => {
+            let reshapeButton = ui.fleRes.$el.find('span#butsf-reshape');
+            if (reshapeButton.length) {
+                reshapeButton.trigger('click');
+            }
+        }, 'Shortcut per il reshape');
+        
         let $btnchs = ui.fleChs.$el;
         $btnchs.append(`<label>
                         <input type="file"
                         accept=".omv,.csv,.dta,.jasp,.Rdata,.sav,.sas7bdat"
                         style="display: none;"/>
-                        <span id="butsf" style="font-size: 1.3em;">Select file ...</span>
+                        <span id="butsf-file" class="button-style" style="font-size: 1.3em;">Select <span>F</span>ile ...</span>
                         </label>`);
         $btnchs.on("change", (f) => {
             var crrTxt = ""; 
             for (const crrFle of f.target.files) {
                 crrTxt += `${crrFle.path}; `;
-
-            };
+            }
             ui.fleInp.setValue(crrTxt.slice(0, -2));
-            
         });
-
-    },
     
-    fleRes_creating: function(ui) {
-
         let $btnres = ui.fleRes.$el;
         $btnres.append(`<label>
                         <input type="button" 
                         style="display: none;"/>
-                        <span id="butsf" style="font-size: 1.3em;">Reshape</span>
+                        <span id="butsf-reshape" class="button-style" style="font-size: 1.3em;"><span>R</span>eshape</span>
                         </label>`);
         $btnres.on('click', () => { 
-                ui.btnReshape.setValue(true);
+            ui.btnReshape.setValue(true);
+        });
+        
+        this.getColumnNames = async () => {
+            try {
+                const data = await this.requestData('columns', {});
+                return data.columns.filter(col => !/^Filter [1-9][0-9]*$/.test(col.name) && !/^F[1-9][0-9]* \([1-9][0-9]*\)$/.test(col.name)).map(col => col.name);
+            } catch (error) {
+                console.error('Error fetching column names:', error);
+            }
+        };
+
+        this.getColumnNames().then(columns => {
+            ui.varAll.setValue(columns);
+        });
+
+        // Funzione per creare e gestire il tooltip
+        function addCustomTooltip(element, text, position) {
+            const tooltip = document.createElement('div');
+            tooltip.className = 'custom-tooltip';
+            tooltip.textContent = text;
+            document.body.appendChild(tooltip);
+
+            element.on('mouseover', (event) => {
+                tooltip.style.display = 'block';
+                if (position === 'left') {
+                    tooltip.style.left = `${event.pageX - tooltip.offsetWidth - 10}px`; // Sposta a sinistra
+                } else {
+                    tooltip.style.left = `${event.pageX + 10}px`; // Sposta a destra
+                }
+                tooltip.style.top = `${event.pageY + 10}px`;
+            });
+
+            element.on('mousemove', (event) => {
+                tooltip.style.top = `${event.pageY + 10}px`;
+            });
+
+            element.on('mouseout', () => {
+                tooltip.style.display = 'none';
+            });
+        }
+
+        // Aggiungi i tooltip personalizzati
+        addCustomTooltip($btnchs.find('#butsf-file'), 'Click or (Ctrl+F) to select a file to merge', 'right');
+        addCustomTooltip($btnres.find('#butsf-reshape'), 'Click or (Ctrl+R) for a new merged file', 'left');
+    },
+
+    update(ui) {
+        this.getColumnNames().then((columns) => {
+            if (!_.isEqual(ui.varAll.value(), columns)) {
+                ui.varAll.setValue(columns);
+            }
+        });
+    },
+
+    dataChanged(ui, event) {
+        if (event.dataType !== 'columns') return;
+
+        this.getColumnNames().then((columns) => {
+            if (!_.isEqual(ui.varAll.value(), columns)) {
+                ui.varAll.setValue(columns);
+            }
         });
     }
-
 };
+
